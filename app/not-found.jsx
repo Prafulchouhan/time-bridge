554a0d5c0822ff17defee98f7ba40e4d6456e8c0
Praful Chouhan <prafulchouhan01@gmail.com>
2024-10-04 01:28:31 +0530

    fixing deployment bockers

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -9,10 +9,12 @@ const NotFound = () => {
             <div className="w-full lg:w-1/2 mx-8">
                 <div className="text-7xl text-green-500 font-dark font-extrabold mb-8"> 404</div>
             <p className="text-2xl md:text-3xl font-light leading-normal mb-8">
-                Sorry we couldn't find the page you're looking for
+                Sorry we couldn not find the page you are looking for
             </p>
             
-            <Link href='/' className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-red-600 hover:bg-red-700">back to homepage</Link>
+            <Link href='/' className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-green-600 active:bg-red-600 hover:bg-red-700">
+            back to homepage
+            </Link>
     </div>
         <div className="lg:flex lg:justify-end lg:w-1/2 mx-5 my-12 relative w-full max-w-md aspect-square">
         <Image src="https://user-images.githubusercontent.com/43953425/166269493-acd08ccb-4df3-4474-95c7-ad1034d3c070.svg"  layout="fill"
